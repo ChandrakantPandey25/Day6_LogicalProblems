f124b58d30682475d20d967934829d9d31bf026b
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 15:03:24 -0700

    Added Fibonacci Series

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Problems");
+            Console.WriteLine("Enter 1 for Fibonacci Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Number");
+                    break;
+            }
+                
+        }
+    }
+}
